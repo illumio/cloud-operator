36ac0a50e8cc9ea9e435c809a2a7515833d0bac4
evanj80-illumio <evan.chopra@illumio.com>
2024-08-07 15:51:49 -0700

    Update integration_tests.sh

--- cloud-operator/integration_tests.sh
+++ cloud-operator/integration_tests.sh
@@ -5,15 +5,15 @@ kind create cluster
 
 
 helm package .
-helm install cloud-operator-0.1.0.tgz . --namespace illumio-cloud --create-namespace
+helm install cloud-operator-0.0.1.tgz . --namespace illumio-cloud --create-namespace
 
 # INSERT KEY CREATION KUBECTL COMMAND HERE IN ORDER TO ACCESS PRIVATE DOCKERHUB REPO 
 
 # Wait for the deployment to be ready
-kubectl rollout status deployment/cloud-operator-0.1.0.tgz-cloud-operator -n illumio-cloud
+kubectl rollout status deployment/cloud-operator-0.0.1.tgz-cloud-operator -n illumio-cloud
 
 # Verify the deployment status
-DEPLOYMENT_STATUS=$(kubectl get deployment cloud-operator-0.1.0.tgz-cloud-operator -n illumio-cloud -o jsonpath="{.status.conditions[?(@.type=='Available')].status}")
+DEPLOYMENT_STATUS=$(kubectl get deployment cloud-operator-0.0.1.tgz-cloud-operator -n illumio-cloud -o jsonpath="{.status.conditions[?(@.type=='Available')].status}")
 if [ "$DEPLOYMENT_STATUS" != "True" ]; then
   echo "Deployment is not available"
   exit 1
