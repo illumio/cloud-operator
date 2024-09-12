cf81899f629027f61b72b8810975130da67d6aaf
Evan <evan.chopra@illumio.com>
2024-09-11 17:17:28 -0700

    Update logger_manager.go

--- internal/controller/logger_manager.go
+++ internal/controller/logger_manager.go
@@ -220,7 +220,7 @@ func NewGRPClogger(grpcSyncer *BufferedGrpcWriteSyncer) *zap.SugaredLogger {
 			return nil
 		}
 
-		// flush buffer so logs are sent in order to the server
+		// Flush buffer so logs are sent in order to the server
 		grpcSyncer.flush()
 
 		if err := grpcSyncer.sendLog(&entry); err != nil {
