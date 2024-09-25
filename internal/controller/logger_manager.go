fe55e92867cbd1c75da76ead8b1637c9cf61f335
aaron.nguyen <aaron.nguyen@illumio.com>
2024-09-25 15:29:26 -0700

    lint

--- internal/controller/logger_manager.go
+++ internal/controller/logger_manager.go
@@ -150,7 +150,7 @@ func (b *BufferedGrpcWriteSyncer) UpdateClient(client pb.KubernetesInfoService_S
 // depending response contents
 func (b *BufferedGrpcWriteSyncer) ListenToLogStream() error {
 	for {
-		//nolint:gosimple // we need to use select because of future expansion to multiple cases
+		//nolint:gosimple,staticcheck // we need to use select because of future expansion to multiple cases
 		select {
 		default:
 			res, err := b.client.Recv()
