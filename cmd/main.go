ec5825cff0b81920a36c531d957aa66713903b64
Evan Chopra <evan.chopra@illumio.com>
2025-02-20 14:33:19 -0800

    cleaning up

--- cmd/main.go
+++ cmd/main.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"errors"
 	"net/http"
-	"time"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
