344536adfd855d7e380b55bbca7070c0f35f67d7
Romain Lenglet <romain.lenglet@berabera.info>
2025-09-30 15:14:52 -0700

    Bump dependencies (#286)
    Install Buf 1.57.2 in devcontainer.
    
    Bump GolangCI Lint from 2.3.1 to 2.5.0.
    Install golangci-lint 2.5.0 in devcontainer.
    Bump golangci-lint in GitHub workflow.
    Fix new linter warnings.
    
    Bump Helm from 3.18.5 to 3.19.0.    
    Install helm 3.19.0 in devcontainer.
    Bump helm in GitHub workflow.
    
    Bump Kind from 0.29.0 to 0.30.0.
    
    Bump Buf Protobuf plugin from 1.36.7 to 1.36.9.
    
    Bump google.golang.org/grpc from 1.74.2 to 1.75.1.
    
    Bump k8s API packages from 0.33.4 to 0.34.1.    
    k8s.io/api
    k8s.io/apimachinery
    k8s.io/client-go
    
    Fix staticcheck warnings.

--- internal/controller/testhelper/set_up_test_cluster.go
+++ internal/controller/testhelper/set_up_test_cluster.go
@@ -7,14 +7,14 @@ import (
 	"os/exec"
 )
 
-// setupTestCluster creates a new KIND cluster for testing.
+// SetupTestCluster creates a new KIND cluster for testing.
 func SetupTestCluster() error {
 	cmd := exec.CommandContext(context.Background(), "kind", "create", "cluster", "--name", "my-test-cluster", "--config", "../../kind-config.yaml")
 
 	return cmd.Run()
 }
 
-// tearDownTestCluster destroys the KIND test cluster.
+// TearDownTestCluster destroys the KIND test cluster.
 func TearDownTestCluster() error {
 	cmd := exec.CommandContext(context.Background(), "kind", "delete", "cluster", "--name", "my-test-cluster")
 
