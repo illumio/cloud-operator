56ff0d2b8c40d98d1ce5f3701598b5ee5e4969df
Sungkyun Chung <sungkyun.chung@illumio.com>
2026-04-06 12:20:32 -0700

    add simple policy mutation handler

---
+++ internal/controller/stream/config/config.go
@@ -0,0 +1,141 @@
+// Copyright 2026 Illumio, Inc. All Rights Reserved.
+
+package config
+
+import (
+	"context"
+	"errors"
+	"io"
+	"time"
+
+	"go.uber.org/zap"
+
+	pb "github.com/illumio/cloud-operator/api/illumio/cloud/k8sclustersync/v1"
+	"github.com/illumio/cloud-operator/internal/controller/stream"
+	"github.com/illumio/cloud-operator/internal/pkg/timeutil"
+)
+
+// Stream handles the configuration update stream.
+func Stream(ctx context.Context, sm *stream.Manager, logger *zap.Logger, keepalivePeriod time.Duration) error {
+	// Initialize GVR cache for policy enforcement
+	if sm.K8sClient != nil {
+		if err := InitPolicyGVRCache(sm.K8sClient.GetClientset(), logger); err != nil {
+			logger.Warn("Failed to initialize policy GVR cache", zap.Error(err))
+		}
+	}
+
+	errCh := make(chan error, 1)
+	defer close(errCh)
+
+	go func() {
+		for {
+			resp, err := sm.Client.ConfigurationStream.Recv()
+			if errors.Is(err, io.EOF) {
+				logger.Info("Server closed the GetConfigurationUpdates stream")
+
+				errCh <- nil
+
+				return
+			}
+
+			if err != nil {
+				logger.Error("Stream terminated", zap.Error(err))
+
+				errCh <- err
+
+				return
+			}
+
+			switch update := resp.GetResponse().(type) {
+			case *pb.GetConfigurationUpdatesResponse_UpdateConfiguration:
+				logger.Info("Received configuration update",
+					zap.Stringer("log_level", update.UpdateConfiguration.GetLogLevel()),
+				)
+
+				if sm.VerboseDebugging {
+					logger.Debug("verboseDebugging is true, setting log level to debug")
+					sm.BufferedGrpcSyncer.UpdateLogLevel(pb.LogLevel_LOG_LEVEL_DEBUG)
+				} else {
+					sm.BufferedGrpcSyncer.UpdateLogLevel(update.UpdateConfiguration.GetLogLevel())
+				}
+
+			case *pb.GetConfigurationUpdatesResponse_NetworkPolicyData:
+				logger.Info("Received network policy data",
+					zap.String("id", update.NetworkPolicyData.GetId()),
+				)
+
+			case *pb.GetConfigurationUpdatesResponse_NetworkPolicySnapshotComplete:
+				logger.Info("Received network policy snapshot complete")
+
+			case *pb.GetConfigurationUpdatesResponse_NetworkPolicyMutation:
+				mutation := update.NetworkPolicyMutation
+				switch m := mutation.GetMutation().(type) {
+				case *pb.NetworkPolicyMutation_CreatePolicy:
+					if err := CreatePolicy(ctx, sm.K8sClient.GetDynamicClient(), logger, m.CreatePolicy); err != nil {
+						logger.Error("Failed to create policy", zap.Error(err))
+					}
+				case *pb.NetworkPolicyMutation_UpdatePolicy:
+					// Update = delete + create
+					if err := DeletePolicy(ctx, sm.K8sClient.GetDynamicClient(), logger, m.UpdatePolicy); err != nil {
+						logger.Error("Failed to delete policy for update", zap.Error(err))
+					}
+					if err := CreatePolicy(ctx, sm.K8sClient.GetDynamicClient(), logger, m.UpdatePolicy); err != nil {
+						logger.Error("Failed to create policy for update", zap.Error(err))
+					}
+				case *pb.NetworkPolicyMutation_DeletePolicy:
+					if err := DeletePolicy(ctx, sm.K8sClient.GetDynamicClient(), logger, m.DeletePolicy); err != nil {
+						logger.Error("Failed to delete policy", zap.Error(err))
+					}
+				}
+
+			default:
+				logger.Warn("Received unknown configuration update", zap.Any("response", resp))
+			}
+		}
+	}()
+
+	ticker := time.NewTicker(timeutil.JitterTime(keepalivePeriod, 0.10))
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case err := <-errCh:
+			if err != nil {
+				return err
+			}
+
+			return nil
+		case <-ticker.C:
+			err := sm.SendKeepalive(logger, stream.TypeConfiguration)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+// ConnectAndStream creates a configuration update stream client and listens for configuration changes.
+func ConnectAndStream(sm *stream.Manager, logger *zap.Logger, keepalivePeriod time.Duration) error {
+	configCtx, configCancel := context.WithCancel(context.Background())
+	defer configCancel()
+
+	getConfigurationUpdatesStream, err := sm.Client.GrpcClient.GetConfigurationUpdates(configCtx)
+	if err != nil {
+		logger.Error("Failed to connect to server", zap.Error(err))
+
+		return err
+	}
+
+	sm.Client.ConfigurationStream = getConfigurationUpdatesStream
+
+	err = Stream(configCtx, sm, logger, keepalivePeriod)
+	if err != nil {
+		logger.Error("Configuration update stream encountered an error", zap.Error(err))
+
+		return err
+	}
+
+	return nil
+}
---
+++ internal/controller/stream/config/policy.go
@@ -0,0 +1,145 @@
+// Copyright 2026 Illumio, Inc. All Rights Reserved.
+
+package config
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"go.uber.org/zap"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+
+	pb "github.com/illumio/cloud-operator/api/illumio/cloud/k8sclustersync/v1"
+)
+
+// policyGVRCache defines supported policy kinds and caches their GVR after discovery.
+var policyGVRCache = map[string]schema.GroupVersionResource{
+	"CiliumNetworkPolicy":            {},
+	"CiliumClusterwideNetworkPolicy": {},
+	"NetworkPolicy":                  {},
+}
+
+// apiGroupVersions maps API groups to their versions for non-v1 resources.
+var apiGroupVersions = map[string]string{
+	"cilium.io": "v2",
+}
+
+func getVersionForGroup(group string) string {
+	if version, ok := apiGroupVersions[group]; ok {
+		return version
+	}
+	return "v1"
+}
+
+func InitPolicyGVRCache(clientset kubernetes.Interface, logger *zap.Logger) error {
+	discoveryClient := clientset.Discovery()
+
+	apiGroups, err := discoveryClient.ServerGroups()
+	if err != nil {
+		logger.Error("Error fetching API groups", zap.Error(err))
+		return err
+	}
+
+	for _, group := range apiGroups.Groups {
+		for _, version := range group.Versions {
+			resourceList, err := discoveryClient.ServerResourcesForGroupVersion(version.GroupVersion)
+			if err != nil {
+				if apierrors.IsForbidden(err) {
+					continue
+				}
+				return err
+			}
+
+			for _, resource := range resourceList.APIResources {
+				if _, exists := policyGVRCache[resource.Kind]; exists {
+					policyGVRCache[resource.Kind] = schema.GroupVersionResource{
+						Group:    group.Name,
+						Version:  getVersionForGroup(group.Name),
+						Resource: resource.Name,
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// getGVR returns the cached GroupVersionResource for a kind.
+func getGVR(kind string) (schema.GroupVersionResource, error) {
+	gvr, ok := policyGVRCache[kind]
+	if !ok || gvr.Resource == "" {
+		return schema.GroupVersionResource{}, fmt.Errorf("unknown or unsupported kind: %s", kind)
+	}
+	return gvr, nil
+}
+
+// CreatePolicy applies a network policy to the cluster.
+func CreatePolicy(ctx context.Context, dynamicClient dynamic.Interface, logger *zap.Logger, policyData *pb.NetworkPolicyData) error {
+	var obj unstructured.Unstructured
+	if err := json.Unmarshal(policyData.GetResource(), &obj.Object); err != nil {
+		return fmt.Errorf("failed to unmarshal policy JSON: %w", err)
+	}
+
+	gvr, err := getGVR(policyData.GetKind())
+	if err != nil {
+		return err
+	}
+
+	namespace := policyData.GetNamespace()
+
+	logger.Info("Creating network policy",
+		zap.String("id", policyData.GetId()),
+		zap.String("name", policyData.GetName()),
+		zap.String("namespace", namespace),
+		zap.String("kind", policyData.GetKind()),
+	)
+
+	if namespace == "" {
+		_, err = dynamicClient.Resource(gvr).Create(ctx, &obj, metav1.CreateOptions{})
+	} else {
+		_, err = dynamicClient.Resource(gvr).Namespace(namespace).Create(ctx, &obj, metav1.CreateOptions{})
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to create policy %s: %w", policyData.GetId(), err)
+	}
+
+	return nil
+}
+
+// DeletePolicy removes a network policy from the cluster.
+func DeletePolicy(ctx context.Context, dynamicClient dynamic.Interface, logger *zap.Logger, policyData *pb.NetworkPolicyData) error {
+	gvr, err := getGVR(policyData.GetKind())
+	if err != nil {
+		return err
+	}
+
+	namespace := policyData.GetNamespace()
+	name := policyData.GetName()
+
+	logger.Info("Deleting network policy",
+		zap.String("id", policyData.GetId()),
+		zap.String("name", name),
+		zap.String("namespace", namespace),
+		zap.String("kind", policyData.GetKind()),
+	)
+
+	if namespace == "" {
+		err = dynamicClient.Resource(gvr).Delete(ctx, name, metav1.DeleteOptions{})
+	} else {
+		err = dynamicClient.Resource(gvr).Namespace(namespace).Delete(ctx, name, metav1.DeleteOptions{})
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to delete policy %s: %w", policyData.GetId(), err)
+	}
+
+	return nil
+}
